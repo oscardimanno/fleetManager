6d75b5b5cd7915bb9692256337a96c634b897292
Oscar Di Manno <oscar.dimanno1@gmail.com>
2023-05-09 20:18:29 +0200

    WIP: Prove del Model, del Router e dei Middleware

--- app/index.php
+++ app/index.php
@@ -2,20 +2,47 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/config.php';
 global $mysql;
+
+
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 $mysql = new \App\classes\mysql();
-
-
 $router = new \App\classes\router();
 
-$router->get('/test', function () {
-    //TODO: implementare correttamente il router
-    return "test";
+$router->get('/veicolo/crea', function ($data = []) {
+    $Veicolo = new \App\models\Veicolo();
+    $Veicolo->find(2);
+    $Veicolo->setMarca("Fiat");
+    $Veicolo->update();
 });
 
+$router->get('/get/{id}', function ($data = []) {
+    $Veicolo = new \App\models\Veicolo();
+    $Veicolo->find(2);
+    $Veicolo->setMarca("Fiat");
+    $Veicolo->update();
+});
 
+$router->middleware = [
+    function ($request, $response) {
+        $response->setContent("Middleware");
+        return [$request, $response];
+    },
+
+    function ($request, $response) {
+        if($response->getContent() == "Middleware"){
+         $response->setContent($response->getContent()."<br>Middleware2");
+        }
+        return [$request, $response];
+    },
+
+    function ($request, $response) {
+    $response->setContent($response->getContent().$request->getMethod() . $request->getRequestUri());
+        $response->send();
+        return [$request, $response];
+    }
+    ];
 
 $router->run();
