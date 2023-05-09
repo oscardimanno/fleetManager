c4cdf0ff5c36a39f8fd4dbccdd072108fbcc27f9
Oscar Di Manno <oscar.dimanno1@gmail.com>
2023-05-09 20:16:56 +0200

    Add: Aggiunge metodi magici nella documentazione

--- app/models/Veicolo.php
+++ app/models/Veicolo.php
@@ -5,6 +5,26 @@ namespace App\models;
 use \App\classes\abstract\Model;
 use \App\classes\mysql;
 
+
+/**
+ * Class Veicolo
+ * @package App\models
+ * @property int $id
+ * @property string $marca
+ * @property string $modello
+ * @property int $cilindrata
+ * @property int $cavalli
+ * @method int getId()
+ * @method string getMarca()
+ * @method string getModello()
+ * @method int getCilindrata()
+ * @method int getCavalli()
+ * @method bool setId(int $id)
+ * @method bool setMarca(string $marca)
+ * @method bool setModello(string $modello)
+ * @method bool setCilindrata(int $cilindrata)
+ * @method bool setCavalli(int $cavalli)
+ */
 class Veicolo extends Model
 {
 
