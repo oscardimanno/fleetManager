574c758e53950d845054171fe658b097cb931be7
Oscar Di Manno <oscar.dimanno1@gmail.com>
2023-05-10 22:08:56 +0200

    Fix: Refactoring e aggiunge errori solo su produzione

--- app/index.php
+++ app/index.php
@@ -4,9 +4,30 @@ require __DIR__ . '/../app/config.php';
 global $mysql;
 
 
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+if (_ENV === "dev") {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PlainTextHandler());
+    $whoops->register();
+}
+
+
+/*
+ * TODO:
+ * 1) autenticazione
+ * 2) sessioni
+ * 3) permessi
+ * 4) cronjob
+ * 5) mail
+ * 6) log
+ * 7) template
+ * 8) form
+ * 9) validation
+ * 10) query builder
+ * 11) test
+ */
 
 $mysql = new \App\classes\mysql();
 $router = new \App\classes\router();
@@ -24,25 +45,5 @@ $router->get('/get/{id}', function ($data = []) {
     $Veicolo->setMarca("Fiat");
     $Veicolo->update();
 });
-
-$router->middleware = [
-    function ($request, $response) {
-        $response->setContent("Middleware");
-        return [$request, $response];
-    },
-
-    function ($request, $response) {
-        if($response->getContent() == "Middleware"){
-         $response->setContent($response->getContent()."<br>Middleware2");
-        }
-        return [$request, $response];
-    },
-
-    function ($request, $response) {
-    $response->setContent($response->getContent().$request->getMethod() . $request->getRequestUri());
-        $response->send();
-        return [$request, $response];
-    }
-    ];
 
 $router->run();
